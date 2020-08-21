b4fa35bb8d2f81ce59aafcb27ae1bf8a1a5848be
LOGGERKID <revanthms.06@gmail.com>
2020-08-21 12:45:12 +0530

    Introduction to redux store

---
+++ src/store/reducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+  counter: 4,
+};
+
+const storeReducer = (state = initialState, action) => {
+  let newState = { ...state };
+  switch (action.type) {
+    case "INC_COUNTER":
+      newState.counter += 1;
+      break;
+    case "DEC_COUNTER":
+      newState.counter -= 1;
+      break;
+    case "ADD_COUNTER":
+      newState.counter += action.value;
+      break;
+    case "SUB_COUNTER":
+      newState.counter -= action.value;
+      break;
+    default:
+      break;
+  }
+  return newState;
+};
+
+export default storeReducer;
