188e1911e7ea69c52333c5fcb3a4df91702ac302
LOGGERKID <revanthms.06@gmail.com>
2020-08-21 14:13:58 +0530

    Ready for Course

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Counter from './containers/Counter/Counter';
-import './App.css';
+import Counter from "./containers/Counter/Counter";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Counter />
+        <Counter />
       </div>
     );
   }
 }
-console.log("test Commit new mint")
 
 export default App;
