bf37973aa0733238211d6ce0879d8e11894af4ac
LOGGERKID <revanthms.06@gmail.com>
2020-08-21 12:59:21 +0530

    Test Udate

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from 'redux'
+import { Provider } from "react-redux";
+import storeReducer from "./store/reducer";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+const store = createStore(storeReducer);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
 });
